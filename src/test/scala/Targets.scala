f11c5d980369c692425006937b785a96048078ab
Chiro <Chiro2001@163.com>
2022-05-08 22:45:51 +0800

    generate `Connect`

--- src/test/scala/Targets.scala
+++ src/test/scala/Targets.scala
@@ -1,13 +1,14 @@
 import modules.DDCMode.DDC_60M
 import modules.DUCMode.DUC_60M
 import modules.{DDC, DUC}
-import top.{Receiver, Sender}
+import top.{Connect, Receiver, Sender}
 
 object Targets {
   val targets = Map(
     "DDC" -> (() => new DDC(mode = DDC_60M)),
     "DUC" -> (() => new DUC(mode = DUC_60M)),
     "Sender" -> (() => new Sender),
-    "Receiver" -> (() => new Receiver)
+    "Receiver" -> (() => new Receiver),
+    "Connect" -> (() => new Connect)
   )
 }

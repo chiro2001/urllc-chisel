59a2726aea5219836ef4ec478f4260bf0eec6055
Chiro <Chiro2001@163.com>
2022-06-09 19:52:52 +0800

    fixed Connet

--- src/main/scala/top/Connect.scala
+++ src/main/scala/top/Connect.scala
@@ -1,13 +1,22 @@
 package top
 
 import chisel3._
-import utils.DataWithSyncWrapper
+import utils.{DataWithSyncWrapper, Utils}
 
 class Connect extends Module {
-  val sender = Module(new Sender)
-  val receiver = Module(new Receiver)
+  val cnt = RegInit(0.U(3.W))
+  Utils.counter(cnt, 2)
+  val slowerClock = (cnt === 0.U).asClock
   val io = IO(new DataWithSyncWrapper)
-  sender.io.in := io.in
-  receiver.io.in := sender.io.out
-  io.out := receiver.io.out
+  val sender = Module(new Sender)
+  val slowerReset = RegInit(true.B)
+  when(cnt === 1.U) {
+    slowerReset := false.B
+  }
+  withClockAndReset(slowerClock, slowerReset) {
+    val receiver = Module(new Receiver)
+    sender.io.in := io.in
+    receiver.io.in := sender.io.out
+    io.out := receiver.io.out
+  }
 }
